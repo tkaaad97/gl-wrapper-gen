9988d145330cbe194eac7a902754955ee2bd3f73
tkaaad97 <tkaaad97@gmail.com>
2018-12-22 16:48:40 +0900

    implementing generate group code

--- gl-wrapper-gen/Group.hs
+++ gl-wrapper-gen/Group.hs
@@ -1,9 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes       #-}
 module Group
-    ( genGroupCode
+    ( genEnumsExportCode
+    , genGroupDeclaresCode
+    , genMemberDeclaresCode
     , parseGroup
-    , writeGroupCode
+    , writeAll
+    , writeEnumExportCode
+    , writeGroupDeclaresCode
+    , writeMemberDeclaresCodes
     ) where
 
 import qualified Data.Text as T (Text, intercalate, unpack)
@@ -20,32 +25,75 @@ parseGroup elem = do
     let members = elem ^.. el "group" ./ el "enum" . attr "name"
     return $ Types.Group name members
 
-writeGroupCode :: Types.Group -> IO ()
-writeGroupCode group = do
-    let code = genGroupCode group
-        groupName = Types.groupName group
-        path = "gl-wrapper/GLW/Internal/Enums/" ++ T.unpack groupName ++ ".hs"
+writeAll :: [Types.Group] -> IO ()
+writeAll groups = do
+    writeEnumExportCode groups
+    writeGroupDeclaresCode groups
+    writeMemberDeclaresCodes groups
+
+writeEnumExportCode :: [Types.Group] -> IO ()
+writeEnumExportCode groups =
+    let groupNames = map Types.groupName groups
+        code = genEnumsExportCode groupNames
+        path = "gl-wrapper/GLW/Enums.hs"
+    in LT.writeFile path code
+
+genEnumsExportCode :: [T.Text] -> LT.Text
+genEnumsExportCode groupNames =
+    [lt|module GLW.Enums
+    ( #{T.intercalate "\n    , " groupNames}
+    ) where
+
+import GLW.Internal.Enums
+|]
+
+writeGroupDeclaresCode :: [Types.Group] -> IO ()
+writeGroupDeclaresCode group = do
+    let code = genGroupDeclaresCode group
+        path = "gl-wrapper/GLW/Internal/Enums.hs"
     LT.writeFile path code
 
-genGroupCode :: Types.Group -> LT.Text
-genGroupCode group =
+genGroupDeclaresCode :: [Types.Group] -> LT.Text
+genGroupDeclaresCode groups =
+    let groupNames = map Types.groupName groups
+        groupDeclares = map genGroupDeclare groupNames
+    in [lt|module GLW.Internal.Enums
+    ( #{T.intercalate "\n    , " groupNames}
+    ) where
+
+import qualified Graphics.GL.Types as GL (GLenum)
+
+#{LT.intercalate "\n" groupDeclares}|]
+
+genGroupDeclare :: T.Text -> LT.Text
+genGroupDeclare groupName = [lt|newtype #{groupName} = #{groupName}
+    { un#{groupName} :: GL.GLenum
+    } deriving (Show, Read, Eq)
+|]
+
+writeMemberDeclaresCodes :: [Types.Group] -> IO ()
+writeMemberDeclaresCodes = mapM_ writeMemberDeclaresCode
+
+writeMemberDeclaresCode :: Types.Group -> IO ()
+writeMemberDeclaresCode group =
     let groupName = Types.groupName group
         groupMembers = Types.groupMembers group
-        memberDeclares = map (genMemberDeclare groupName) groupMembers
-    in [lt|module GLW.Internal.Enums.#{groupName}
-    ( #{groupName}(..)
+        code = genMemberDeclaresCode groupName groupMembers
+        path = "gl-wrapper/GLW/Enums/" ++ T.unpack groupName ++ ".hs"
+    in LT.writeFile path code
+
+genMemberDeclaresCode :: T.Text -> [T.Text] -> LT.Text
+genMemberDeclaresCode groupName groupMembers =
+    let memberDeclares = map (genMemberDeclare groupName) groupMembers
+    in [lt|module GLW.Enums.#{groupName}
+    ( #{groupName}
     , #{T.intercalate "\n    , " groupMembers}
     ) where
 
 import qualified Graphics.GL.Internal.Shared as GL (#{T.intercalate ", " groupMembers})
-import qualified Graphics.GL.Types as GL (GLenum)
+import GLW.Internal.Enums (#{groupName}(..))
 
-newtype #{groupName} = #{groupName}
-    { un#{groupName} :: GL.GLenum
-    } deriving (Show, Read, Eq)
-
-#{LT.intercalate "\n" memberDeclares}
-|]
+#{LT.intercalate "\n" memberDeclares}|]
 
 genMemberDeclare :: T.Text -> T.Text -> LT.Text
 genMemberDeclare group member = [lt|#{member} :: #{group}
--- gl-wrapper-gen/Main.hs
+++ gl-wrapper-gen/Main.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Control.Exception (throwIO)
+import Data.Maybe (maybe)
 import qualified Data.Text.Lazy.IO as LT (putStr)
-import Group (genGroupCode, parseGroup)
+import qualified Group (parseGroup, writeAll)
+import System.IO.Error (userError)
 import qualified Text.XML as XML (def, readFile)
 import Text.XML.Lens
 import Types (Group(..))
@@ -10,6 +13,7 @@ import Types (Group(..))
 main :: IO ()
 main = do
     doc <- XML.readFile XML.def "gl.xml"
-    let group = head $ doc ^.. root . el "registry" ./ el "groups" ./ el "group"
-        Just code = genGroupCode <$> parseGroup group
-    LT.putStr code
+    let groupElements = doc ^.. root . el "registry" ./ el "groups" ./ el "group"
+        maybeGroups = mapM Group.parseGroup groupElements
+    groups <- maybe (throwIO . userError $ "failed to parse groups") return maybeGroups
+    Group.writeAll groups
