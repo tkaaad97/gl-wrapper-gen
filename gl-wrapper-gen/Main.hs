a91d3368fff70b6056e01afdf96742103bfc2777
tkaaad97 <tkaaad97@gmail.com>
2018-12-29 13:51:29 +0900

    generate only gl commands

--- gl-wrapper-gen/Main.hs
+++ gl-wrapper-gen/Main.hs
@@ -4,9 +4,9 @@ module Main where
 import qualified Command (parseCommand, writeAll)
 import Control.Exception (throwIO)
 import Control.Lens (filtered, ix)
-import Data.Maybe (maybe)
+import Data.Maybe (isJust, maybe)
 import Data.Set (Set)
-import qualified Data.Set as Set (fromList, union)
+import qualified Data.Set as Set (difference, fromList, member, union)
 import qualified Data.Text.Lazy.IO as LT (putStr)
 import Debug.Trace (trace)
 import qualified Group (parseGroup, writeAll)
@@ -25,7 +25,11 @@ main = do
         enumNames = Set.union featureEnumNames extEnumNames
         groupElements = doc ^.. root . el "registry" ./ el "groups" ./ el "group"
         maybeGroups = mapM (Group.parseGroup enumNames) groupElements
-        commandElements = doc ^.. root . el "registry" ./ el "commands" ./ el "command" . filtered (\x -> x ^? el "command" ./ el "proto" ./ el "name" . text /= Just "glCreateSyncFromCLeventARB")
+        glFeatureCommandNames = Set.fromList $ doc ^.. root . el "registry" ./ el "feature" . attributeIs "api" "gl" ./ el "requre" ./ el "command" . attr "name"
+        removedCommandNames = Set.fromList $ doc ^.. root . el "registry" ./ el "feature" . attributeIs "api" "gl" ./ el "remove" ./ el "command" . attr "name"
+        extensionCommandNames = Set.fromList $ doc ^.. root . el "registry" ./ el "extensions" ./ el "extension" . attributeIs "supported" "gl" ./ el "require" ./ el "command" . attr "name"
+        commandNames = (glFeatureCommandNames `Set.union` extensionCommandNames) `Set.difference` removedCommandNames
+        commandElements = doc ^.. root . el "registry" ./ el "commands" ./ el "command" . filtered (filterCommand commandNames)
     groups <- maybe (throwIO . userError $ "failed to parse groups") return maybeGroups
     let filteredGroups = filter (not . null . Types.groupMembers)  groups
         groupNames = Set.fromList $ map Types.groupName filteredGroups
@@ -35,3 +39,7 @@ main = do
     commands <- maybe (throwIO . userError $ "failed to parse commands") return maybeCommands
     Command.writeAll groupNames commands
     return ()
+
+    where
+    filterCommand commandNames x = isJust $
+        x ^? el "command" ./ el "proto" ./ el "name" . text . filtered (`Set.member` commandNames)
