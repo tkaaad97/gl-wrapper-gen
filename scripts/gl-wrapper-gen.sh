f8f152a2e7812473baa51c1e11aaa858968f0170
tkaaad97 <tkaaad97@gmail.com>
2019-01-01 17:57:33 +0900

    fix script

--- scripts/gl-wrapper-gen.sh
+++ scripts/gl-wrapper-gen.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 find gl-wrapper -type f -name 'GLW.hs' -exec rm {} \;
 find gl-wrapper/GLW -type f -name '*.hs' ! -name 'Classes.hs' -exec rm {} \;
+./scripts/supplement-spec.sh > glw.xml
 docker-compose run --rm app stack run gl-wrapper-gen
 sudo chown "$(id -gn "$USER")":"$USER" -R gl-wrapper
