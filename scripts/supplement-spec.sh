bdaf01a607d6d2cb3abb841054b330470ac14c63
tkaaad97 <tkaaad97@gmail.com>
2019-01-05 21:58:24 +0900

    wrapping some params

--- scripts/supplement-spec.sh
+++ scripts/supplement-spec.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-injects=$(xmlstarlet sel -B -t -c '.' objects.xml)
+injects1=$(xmlstarlet sel -B -t -c '.' objects.xml)
+injects2=$(xmlstarlet sel -B -t -c '.' newtypes.xml)
 spec=$(cat gl.xml)
 
 while read -r inject
@@ -8,6 +9,9 @@ do
     attribute=$(xmlstarlet sel -t -v '/inject/@attribute' <<<"$inject")
     value=$(xmlstarlet sel -t -v '/inject/@value' <<<"$inject")
     spec=$(echo "$spec" | xmlstarlet ed -P -i "$xpath" -t attr -n "$attribute" -v "$value")
-done < <(xmlstarlet sel -t -m '/objects/object/injects/inject' -c . --nl <<<"$injects")
+done < <({
+    xmlstarlet sel -t -m '/objects/object/injects/inject' -c . --nl <<<"$injects1";
+    xmlstarlet sel -t -m '/newtypes/newtype/injects/inject' -c . --nl <<<"$injects2";
+})
 
 echo "$spec"
