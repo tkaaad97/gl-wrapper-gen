5480ea25a984a441a039af7705c2fe2c8f9f267b
tkaaad97 <tkaaad97@gmail.com>
2019-01-13 18:23:12 +0900

    fix inject xml

--- scripts/supplement-spec.sh
+++ scripts/supplement-spec.sh
@@ -5,13 +5,20 @@ spec=$(cat gl.xml)
 
 while read -r inject
 do
-    xpath=$(xmlstarlet sel -t -v '/inject/@xpath' <<<"$inject")
-    attribute=$(xmlstarlet sel -t -v '/inject/@attribute' <<<"$inject")
-    value=$(xmlstarlet sel -t -v '/inject/@value' <<<"$inject")
-    spec=$(echo "$spec" | xmlstarlet ed -P -i "$xpath" -t attr -n "$attribute" -v "$value")
+    command='xmlstarlet ed -P '
+    while read -r action
+    do
+        method=$(xmlstarlet sel -t -v '/action/@method' <<<"$action")
+        xpath=$(xmlstarlet sel -t -v '/action/@xpath' <<<"$action")
+        type=$(xmlstarlet sel -t -v '/action/@type' <<<"$action")
+        name=$(xmlstarlet sel -t -v '/action/@name' <<<"$action")
+        value=$(xmlstarlet sel -t -v '/action/@value' <<<"$action")
+        command+="--${method} '""${xpath}""' -t \"${type}\" -n \"${name}\" -v \"${value}\" "
+    done < <(xmlstarlet sel -t -m '/inject/action' -c '.' --nl <<<"$inject")
+    spec=$(echo "$spec" | bash -c "$command")
 done < <({
-    xmlstarlet sel -t -m '/objects/object/injects/inject' -c . --nl <<<"$injects1";
-    xmlstarlet sel -t -m '/newtypes/newtype/injects/inject' -c . --nl <<<"$injects2";
+    xmlstarlet sel -t -m '/objects/object/injects/inject' -c '.' --nl <<<"$injects1";
+    xmlstarlet sel -t -m '/newtypes/newtype/injects/inject' -c '.' --nl <<<"$injects2";
 })
 
 echo "$spec"
