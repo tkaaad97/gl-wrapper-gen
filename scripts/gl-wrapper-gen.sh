f18c9d0dcbcb7d9b2b2b8d3ed61a8ae714912f89
tkaaad97 <tkaaad97@gmail.com>
2019-11-24 16:37:27 +0900

    change image and lts

--- scripts/gl-wrapper-gen.sh
+++ scripts/gl-wrapper-gen.sh
@@ -6,5 +6,4 @@ find "$OUTPUT_PATH" -type f -name 'GLW.hs' -exec rm {} \;
 find "$OUTPUT_PATH"/GLW -type f -name '*.hs' -exec rm {} \;
 ./scripts/supplement-spec.sh > glw.xml
 docker-compose run --rm app stack run gl-wrapper-gen -- "$OUTPUT_PATH"
-sudo chown "$(id -gn "$USER")":"$USER" -R "$OUTPUT_PATH"
 cp -r "$OUTPUT_PATH" "$COPY_PATH"
